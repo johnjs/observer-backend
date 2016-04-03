a9e212948a839fe6c3a9c0f9f78a0a6875b4027f
johnjs <d.m.michalski@gmail.com>
2016-04-03 11:42:07 +0700

    OB-4 Update project configuration module.
    * renamed `SCRAPING_INTERVAL` parameter to `SCHEDULER_POLLING_INTERVAL`,
    * add parameters used to create a connection with MongoDB.
    * removed unused facebook token & account parameters.

--- server/config/config.js
+++ server/config/config.js
@@ -4,10 +4,8 @@ import globalConfig from './global.json';
 const envConfig = require(`./${process.env.NODE_ENV || 'development'}.json`);
 const config = extend(globalConfig, envConfig);
 
-config.FACEBOOK_ACCOUNT = process.env.FACEBOOK_ACCOUNT || config.FACEBOOK_ACCOUNT;
-config.FACEBOOK_TOKEN = process.env.FACEBOOK_TOKEN || config.FACEBOOK_TOKEN;
-config.FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID || config.FACEBOOK_APP_ID;
-config.FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET || config.FACEBOOK_APP_SECRET;
-config.FEED_DESTINATION = process.env.FEED_DESTINATION || config.FEED_DESTINATION;
+if (process.env.NODE_ENV !== 'test') {
+  extend(config, process.env);
+}
 
 export default config;
