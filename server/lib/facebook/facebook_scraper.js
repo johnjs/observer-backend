f3c036133dcab84465bccba9d54b738bc1bb29c0
johnjs <d.m.michalski@gmail.com>
2016-04-07 12:27:56 +0800

    OB-6 Facebook scraper extends the abstract scraper.

--- server/lib/facebook/facebook_scraper.js
+++ server/lib/facebook/facebook_scraper.js
@@ -9,15 +9,10 @@
  */
 
 import { extend } from 'underscore';
-import jsonfile from 'jsonfile';
-import config from '../../config/config';
-import logger from '../../utils/logger.js';
-import Q from 'q';
 import moment from 'moment';
-import FeedStream from './feed_stream';
-
-const FEED_DIRECTORY = './feed/facebook/';
-const FEED_FILE_FORMATTING_OPTIONS = { spaces: 2 };
+import { isExecutedAsScript } from '../../utils/module_utils';
+import FacebookFeedStream from './facebook_feed_stream';
+import AbstractScraper from '../abstract_scraper';
 
 /**
 * Returns default parameters of Facebook API call. Since we are not interested
@@ -33,79 +28,47 @@ function getDefaultRequestParams() {
   };
 }
 
-export default class FacebookScraper {
+export default class FacebookScraper extends AbstractScraper {
 
   /**
    * @constructor
-   * @param {string} account - name of Facebook account tracked by the scraper
+   * @param {string} accountName - name of Facebook account tracked by the scraper
    * @param {string} token - access token used to access Facebook Graph API
    */
-  constructor(account, token) {
-    this.account = account;
+  constructor(accountName, token) {
+    super(accountName);
     this.token = token;
-  }
-
-  /**
-   * The function uses the data stream to collect Facebook data.
-   */
-  scrape() {
-    const url = this._buildRequestUrl();
-    const stream = this._getDataStream();
-    const feed = [];
-
-    stream.on('error', err => this._failure(err));
-    stream.on('data', chunk => Array.prototype.push.apply(feed, chunk));
-    stream.on('finish', () => this._saveFeed(feed).done(this._success));
-
-    stream.callGraphAPI(url);
-  }
-
-  _failure(error) {
-    logger.logError(error);
-    process.exit(1);
-  }
-
-  _success() {
-    process.exit(0);
   }
 
   /**
    * Creates an instance of the stream returning Facebook data.
    */
   _getDataStream() {
-    return new FeedStream();
+    return new FacebookFeedStream();
   }
 
   /**
-   * Builds the path and the query parts of the url used when Facebook API is called
-   * Example: /manchesterunited/feed?since=XXX&limit=100
-   */
-  _buildRequestUrl() {
+  * Builds the path and the query parts of the url used when Facebook API is called
+  * Example: /manchesterunited/feed?since=XXX&limit=100
+  */
+  _getStreamingOptions() {
     const reqParams = extend({ access_token: this.token }, getDefaultRequestParams());
     const reqParamsStr = Object.keys(reqParams).reduce(
       (p, k) => p.concat(`${k}=${reqParams[k]}`), []).join('&');
-    return `${this.account}/feed?${reqParamsStr}`;
-  }
-
-  _saveFeed(feed) {
-    if (config.FEED_DESTINATION === 'FILE') {
-      return this._saveFeedInFile(feed);
-    }
-    return new Q();
+    return [`${this.accountName}/feed?${reqParamsStr}`];
   }
 
   /**
-   * Saves the feed in JSON file in `/feed/facebook` directory. Name of the file
-   * is built as a concatenation of Facebook account's name and current date time.
-   * @param {object} feed - Facebook posts gathered from Facebook API for a given account
-   */
-  _saveFeedInFile(feed) {
-    const fileName = `${FEED_DIRECTORY}${this.account}_${(new Date()).toISOString()}.json`;
-    return Q.denodeify(jsonfile.writeFile)(fileName, feed, FEED_FILE_FORMATTING_OPTIONS);
+   * Return a relative path to the directory containing feed which was fetched
+   * and saved as a JSON file.
+   * @returns {String} path
+   **/
+  _getFeedDirectory() {
+    return './feed/facebook/';
   }
 }
 
-if (require.main === module) {
+if (isExecutedAsScript(module)) {
   const args = process.argv;
   const scraper = new FacebookScraper(args[2], args[3]);
   scraper.scrape();
