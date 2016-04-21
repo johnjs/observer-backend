1f720cb42f0deceaeda7b0b1e4d0a6bbb562a7c0
johnjs <d.m.michalski@gmail.com>
2016-04-21 20:51:13 +0800

    OB-10 Added the feed output stream factory

---
+++ test/server/lib/feed_output_stream_factory.test.js
@@ -0,0 +1,65 @@
+import { assert } from 'chai';
+import * as sinon from 'sinon';
+import Q from 'q';
+import config from '../../../server/config/config.js';
+import FeedOutputStreamFactory from '../../../server/lib/feed_output_stream_factory';
+import FileFeedOutputStream from '../../../server/lib/file_feed_output_stream';
+import QueueFeedOutputStream from '../../../server/lib/queue_feed_output_stream';
+
+describe('feed_output_stream_factory', () => {
+  let sandbox;
+  const fakeAccount = 'tom_hagen';
+  const fakeSource = 'irish_district';
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('getStream', () => {
+    describe('when the FEED_DESTINATION config variable equals QUEUE', () => {
+      beforeEach(() => {
+        const fakeStream = new QueueFeedOutputStream(fakeAccount, fakeSource);
+        sinon.stub(QueueFeedOutputStream, 'getInstance').returns(new Q(fakeStream));
+      });
+
+      afterEach(() => {
+        QueueFeedOutputStream.getInstance.restore();
+      });
+
+      it('returns a promise resolving to instance of QueueFeedOutputStream', (done) => {
+        sandbox.stub(config, 'get').withArgs('FEED_DESTINATION').returns('QUEUE');
+
+        FeedOutputStreamFactory.getStream(fakeAccount, fakeSource).then((stream) => {
+          assert.instanceOf(stream, QueueFeedOutputStream);
+          done();
+        });
+      });
+    });
+
+    describe('when the FEED_DESTINATION config variable equals FILE', () => {
+      it('returns a promise resolving to instance of FileFeedOutputStream', (done) => {
+        sandbox.stub(config, 'get').withArgs('FEED_DESTINATION').returns('FILE');
+        FeedOutputStreamFactory.getStream(fakeAccount, fakeSource).then((stream) => {
+          assert.instanceOf(stream, FileFeedOutputStream);
+          done();
+        });
+      });
+    });
+
+    describe('when the FEED_DESTINATION config variable ' +
+                ' is not a valid feed destination', () => {
+      it('throws an error', () => {
+        sandbox.stub(config, 'get').withArgs('FEED_DESTINATION').returns('SICILIA');
+        const expectedErrorMsg = 'No streams defined for SICILIA. Use one of: [FILE,QUEUE].';
+
+        assert.throws(() => {
+          FeedOutputStreamFactory.getStream(fakeAccount, fakeSource);
+        }, expectedErrorMsg);
+      });
+    });
+  });
+});
