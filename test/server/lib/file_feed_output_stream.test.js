7a34df528ae41620b7caae99c6efcde1bff5e4b0
johnjs <d.m.michalski@gmail.com>
2016-04-21 13:33:10 +0800

    OB-10 Added the queue feed output stream

--- test/server/lib/file_feed_output_stream.test.js
+++ test/server/lib/file_feed_output_stream.test.js
@@ -54,6 +54,7 @@ describe('file_feed_output_stream', () => {
       const inputDataChunks = [
         [{ id: 1 }, { id: 2 }],
         [{ id: 3 }, { id: 4 }],
+        [],
       ];
       const expectedAggregatedChunks = [
         { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 },
