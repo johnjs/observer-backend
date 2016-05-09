08403b036a41f1118023bdc2c68dc31ba9a1100a
johnjs <d.m.michalski@gmail.com>
2016-05-09 21:42:11 +0800

    OB-18 Used diehard module to close db/queue connections when the app is killed

--- test/lib/storage/clients/queue.test.js
+++ test/lib/storage/clients/queue.test.js
@@ -1,7 +1,9 @@
-import { assert } from 'chai';
-import * as sinon from 'sinon';
+import diehard from 'diehard';
 import Q from 'q';
 import ampq from 'amqplib';
+import { assert } from 'chai';
+import * as sinon from 'sinon';
+import logger from '../../../../lib/utils/logger';
 import * as queue from '../../../../lib/storage/clients/queue';
 
 describe('queue', () => {
@@ -16,17 +18,33 @@ describe('queue', () => {
   });
 
   describe('createConnection', () => {
-    const fakeChannel = { name: 'frederico' };
+    let fakeConnection;
 
     beforeEach(() => {
-      sandbox.stub(ampq, 'connect').returns(new Q(fakeChannel));
+      fakeConnection = {
+        close: sandbox.stub().returns(new Q()),
+      };
+      sandbox.stub(ampq, 'connect').returns(new Q(fakeConnection));
+      sandbox.stub(diehard, 'register');
     });
 
     it('returns a promise resolving with a rabbitmq channel', (done) => {
-      queue.createConnection().then((actualChannel) => {
-        assert.equal(actualChannel, fakeChannel);
+      queue.createConnection().then((actualConnection) => {
+        assert.equal(actualConnection, fakeConnection);
         done();
       });
+    });
+
+    it('registers a diehard listener for `process killed/interrupted` events '
+          + 'which closes the queue connection', (done) => {
+      const expecteMsg = 'RabbitMQ connection disconnected due to app termination';
+      sandbox.stub(logger, 'logInfo');
+
+      diehard.register.callsArgWithAsync(0, () => {
+        assert.ok(logger.logInfo.calledWith(expecteMsg));
+        done();
+      });
+      queue.createConnection();
     });
   });
 });
